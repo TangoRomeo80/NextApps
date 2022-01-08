7db39073458fdc69a5f44b4955628b2dceb4c34a
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-01-08 22:34:32 +0600

    updated home styling

--- discover-coffee-shop/pages/coffee-store/[id].js
+++ discover-coffee-shop/pages/coffee-store/[id].js
@@ -1,4 +1,5 @@
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const CoffeeStore = () => {
   const router = useRouter()
@@ -6,10 +7,15 @@ const CoffeeStore = () => {
   console.log('router', router)
   return (
     <div>
-      Coffee Store Page { router.query.id }
+      Coffee Store Page {router.query.id}
+      <Link href='/'>
+        <a>back to home</a>
+      </Link>
+      <Link href='/coffee-store/dynamic'>
+        <a>Go to page dynamic</a>
+      </Link>
     </div>
   )
 }
 
 export default CoffeeStore
-
