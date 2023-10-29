7a72ad96f74ee842683bf8aa4366736c1298b5cc
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-10-29 14:14:29 +0600

    completed upto moshvid 0055

---
+++ mosh-next-app/app/upload/page.tsx
@@ -0,0 +1,38 @@
+// This is a page for image upload
+'use client'
+import React from 'react'
+import { CldUploadWidget, CldImage } from 'next-cloudinary'
+
+interface CloudinaryResult {
+  public_id: string
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = React.useState('')
+
+  return (
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt='alt image' />
+      )}
+      <CldUploadWidget
+        uploadPreset='ldaj02nv'
+        onUpload={(result, widget) => {
+          if (result.event !== 'success') {
+            return
+          }
+          const info = result.info as CloudinaryResult
+          setPublicId(info.public_id)
+        }}
+      >
+        {({ open }) => (
+          <button className='btn btn-primary' onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  )
+}
+
+export default UploadPage
