016a50da58f45a4d8a619952e6c1e67ad0d1bbef
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-11-01 13:45:37 +0600

    style the active link in navbar of issue tracker

--- issue_tracker/app/NavBar.tsx
+++ issue_tracker/app/NavBar.tsx
@@ -1,8 +1,16 @@
+'use client' // this directive is required for next.js to recognize this file as a client-side file
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames'
 
 const NavBar = () => {
+  // Note: usePathname() is a custom hook from next/navigation that returns the current pathname
+  const currentPath = usePathname()
+
+  // link objects are used to generate the links in the navbar
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
@@ -10,6 +18,7 @@ const NavBar = () => {
 
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+      {/* Link is a next/link component that wraps an anchor tag */}
       <Link href='/'>
         <AiFillBug />
       </Link>
@@ -17,7 +26,12 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            // Change the active link colour using classnames package
+            className={classnames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
             href={link.href}
           >
             {link.label}
