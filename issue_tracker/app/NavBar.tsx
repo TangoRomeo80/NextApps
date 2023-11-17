aa1b16f01e4d319748980da64b713a6defcdbcc6
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-11-17 19:32:21 +0600

    add dropdown menu to show current user in issue tracker

--- issue_tracker/app/NavBar.tsx
+++ issue_tracker/app/NavBar.tsx
@@ -6,7 +6,14 @@ import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import classnames from 'classnames'
 import { useSession } from 'next-auth/react'
-import { Box, Container, Flex } from '@radix-ui/themes'
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from '@radix-ui/themes'
 
 const NavBar = () => {
   // Note: usePathname() is a custom hook from next/navigation that returns the current pathname
@@ -18,6 +25,8 @@ const NavBar = () => {
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues/list' },
   ]
+
+  console.log(session)
 
   return (
     <nav className='border-b mb-5 px-5 py-3'>
@@ -46,7 +55,25 @@ const NavBar = () => {
           </Flex>
           <Box>
             {status === 'authenticated' && (
-              <Link href='/api/auth/signout'>Log out</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback='?'
+                    size='2'
+                    radius='full'
+                    className='cursor-pointer'
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size='2'>{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href='/api/auth/signout'>Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             )}
             {status === 'unauthenticated' && (
               <Link href='/api/auth/signin'>Login</Link>
